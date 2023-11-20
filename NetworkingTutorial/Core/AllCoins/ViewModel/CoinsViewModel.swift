1d39f97f829a3896b3161ec967d05764304dc342
BlueBeard <desaeger@hcri.com>
2023-11-20 12:15:39 -0800

    End of module 2

--- NetworkingTutorial/Core/AllCoins/ViewModel/CoinsViewModel.swift
+++ NetworkingTutorial/Core/AllCoins/ViewModel/CoinsViewModel.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+class CoinsViewModel: ObservableObject {
+    @Published var coin = ""
+    @Published var price = ""
+    
+    init(coin: String = "", price: String = "") {
+       fetchPrice()
+    }
+    func fetchPrice() {
+    let urlString = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
+         
+        guard let url = URL(string: urlString) else { return }
+        print("Fetch price....")
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            print("Did receive data \(data)?")
+            print("Data from JSON \(data!) ")
+        }.resume()
+        
+        print("Did reach the end of function")
+    }
+}
+    
