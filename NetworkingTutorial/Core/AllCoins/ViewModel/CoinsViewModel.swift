02738a13a2d04cf473d8bb8da3a6d95cf59b5e55
BlueBeard <desaeger@hcri.com>
2023-11-19 18:31:12 -0800

    Main Setup

---
+++ NetworkingTutorial/Core/AllCoins/ViewModel/CoinsViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CoinsViewModel.swift
+//  NetworkingTutorial
+//
+//  Created by Robert De Saeger on 11/19/23.
+//
+
+import Foundation
---
+++ NetworkingTutorial/TestOnly.swift
@@ -0,0 +1,18 @@
+//
+//  TestOnly.swift
+//  NetworkingTutorial
+//
+//  Created by Robert De Saeger on 11/19/23.
+//
+
+import SwiftUI
+
+struct TestOnly: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestOnly()
+}
