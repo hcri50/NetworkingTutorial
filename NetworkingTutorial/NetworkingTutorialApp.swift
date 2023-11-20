bd0351361c27fdb21464f7ef46a2b04a89dfd4dd
BlueBeard <desaeger@hcri.com>
2023-11-19 18:22:17 -0800

    Initial Commit

---
+++ NetworkingTutorial/NetworkingTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingTutorialApp.swift
+//  NetworkingTutorial
+//
+//  Created by Robert De Saeger on 11/19/23.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
